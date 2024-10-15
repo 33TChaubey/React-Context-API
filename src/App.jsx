f21b88ad44ee04c68a3c758f7058d046413056eb
Trilokinath Chaubey <chaubeytn2103@outlook.com>
2024-10-15 22:05:12 +0530

    Setup the Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Link, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
+import User from './components/User'
+import About from './components/About'
+import Userdetail from './components/Userdetail'
 import { useState } from 'react'
 const App = () => {
 
   return (
     <div>
+
+      <nav className='flex justify-center gap-10'>
+        <Link to='/'>Home</Link>
+        <Link to='/user'>User</Link>
+        <Link to='/about'>About</Link>
+      </nav>
+      <hr /><hr />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/user' element={<User />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
       <Home />
     </div>
   )
