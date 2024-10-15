d437ac0471123a09d49b17ea5b9e1ef3977e53a1
Trilokinath Chaubey <chaubeytn2103@outlook.com>
2024-10-15 21:34:08 +0530

    Using Context API

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './components/Home'
+import { useState } from 'react'
+const App = () => {
+
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Userdetail from './Userdetail'
+
+const User = () => {
+  return (
+    <div>
+
+      <Userdetail />
+    </div>
+  )
+}
+
+export default User
